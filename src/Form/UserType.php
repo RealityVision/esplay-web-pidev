73d68404387027851e16a1905d749dac5905a909
nadee ben cheikh <nada.bencheikhsalah@esprit.tn>
2022-04-14 02:34:23 +0100

    integration admin

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,23 +6,29 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('phone')
-            ->add('email')
+            ->add('username',TextType::class)
+            ->add('firstName',TextType::class)
+            ->add('lastName',TextType::class)
+            ->add('phone',NumberType::class)
+            ->add('email',EmailType::class)
             ->add('password')
             ->add('salt')
-            ->add('country')
+            ->add('country',TextType::class)
             ->add('birthDate')
-            ->add('picture')
-            ->add('address')
+            ->add('picture',FileType::class)
+            ->add('address',TextType::class)
             ->add('gender')
             ->add('role')
         ;
